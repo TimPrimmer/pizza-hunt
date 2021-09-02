9f9a850cd1ba6084957cf16579ef8ccd9da6081d
Tim Primmer <timprimmer@gmail.com>
2021-09-02 17:50:26 -0500

    created reply schema and updated comment routes

--- routes/api/comment-routes.js
+++ routes/api/comment-routes.js
@@ -1,11 +1,22 @@
 const router = require('express').Router();
-const { addComment, removeComment } = require('../../controllers/comment-controller');
+const {
+  addComment,
+  removeComment,
+  addReply,
+  removeReply
+} = require('../../controllers/comment-controller');
 
 
 // /api/comments/<pizzaId>
 router.route('/:pizzaId').post(addComment);
 
 // /api/comments/<pizzaId>/<commentId>
-router.route('/:pizzaId/:commentId').delete(removeComment);
+router
+  .route('/:pizzaId/:commentId')
+  .put(addReply) // the callback function of a route method has req and res as parameters, so we don't have to explicitly pass any arguments
+  .delete(removeComment)
+
+
+router.route('/:pizzaId/:commentId/:replyId').delete(removeReply);
 
 module.exports = router;
